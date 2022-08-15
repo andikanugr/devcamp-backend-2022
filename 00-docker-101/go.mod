0008f7b7fe8f0329a5f343156baa0a4bb163c2e5
Andika Nugraha <andika.nugraha@tokopedia.com>
2022-08-15 21:56:58 +0700

    Add Dockerfile & simple app example

---
+++ 00-docker-101/go.mod
@@ -0,0 +1,3 @@
+module 00-docker-101
+
+go 1.17
---
+++ 00-docker-101/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	env := "LOCAL"
+	if value, ok := os.LookupEnv("ENV"); ok {
+		env = value
+	}
+	fmt.Println("Hello Tokopedia Devcamp 2022 participants!")
+	fmt.Printf("This message is run on %s environtment\n", env)
+}
